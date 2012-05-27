27952cbf87f15d3bb1503bc569b9cb4ee574b0ba
swair shah <swairshah@gmail.com>
2012-05-27 18:55:48 +0530

    populating db with rs_scrape data

--- lib/scraper/rs_scraper.rb
+++ lib/scraper/rs_scraper.rb
@@ -14,12 +14,39 @@ sessions << "219Part1" << "219Part2"
 sessions << "225Part1" << "225Part2"
 
 sessions.each do |session|
+
   url = rs_session_url_head+session
   doc = Nokogiri::HTML(open(url))
+
+  total_sittings = doc.search('#lb_totaldays').text
+  rs_session = RsSession.find_or_create_by_session_number(session)
+  rs_session.update_attribute('total_sittings',total_sittings.to_i)
+  
+
+
   table = doc.search('#GridView1 tr')
   table.search('tr').each do |tr|
+    data = {}
     tr.search('td').each_with_index do |td,i|
-      p td.content if i==2
+      case i 
+      when 1 then 
+        division_number = td.content.strip
+        data[:division_number] =division_number
+      when 2 then
+        name = td.content.strip
+        data[:name] = name
+      when 3 then
+        state = td.content.strip
+        data[:state] = state
+      when 4 then
+        value = td.content.strip
+        data[:value] = value
+      end
     end
+    rs_member = RsMember.find_or_create_by_division_number(data[:division_number].to_i)
+    rs_member.update_attributes(:name => data[:name],:state => data[:state])
+    rs_member.rs_attendances << rs_session.rs_attendances.create(:value => data[:value]) 
+
+
   end
 end
