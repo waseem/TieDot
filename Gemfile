ee6e7d42cc3612e21ec4da62e8d2f00ee6cbb0a7
Jigar Patel <jagira@gmail.com>
2012-05-27 15:02:14 +0530

    Loksabha Scraper / Parser 1.0
    Print the output in console. Need to dump the data in db.

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ end
 gem 'jquery-rails'
 
 gem 'watir-webdriver'
+gem 'nokogiri'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- lib/scraper/ls_scraper_v1.rb
+++ lib/scraper/ls_scraper_v1.rb
@@ -1,21 +1,83 @@
 require 'watir-webdriver' 
 require 'uri' 
+require 'nokogiri'
 
+# initialize browser
 browser = Watir::Browser.new :firefox 
 
-ls_site = "http://164.100.47.132/Members_Attendance/datewise_results.aspx?xlok=15&vsession=4&vdoa=5/7/2010"
+# parliaments collection - current is the 15th parliament
+parliaments = [15]
 
-browser.goto ls_site
+# parliament sessions
+parliament_sessions = {}
+# sessions of 15th parliament
+parliament_sessions["15"] = [1,2,3,4,5,6,7,8,9,10]
 
-total_links = browser.body.table(:id, "DataGrid1").rows.first.as.size
 
-body = browser.body
+parliaments.each do |parliament|
 
-(1..total_links).each do |i|
-  body.table(:id,"DataGrid1").rows[2].tds.each {|td| p td.html}
-  next_link = body.table(:id,"DataGrid1").rows.first.as[i-1]
-  next_link.click
-  body = browser.body
+  sessions = parliament_sessions[parliament.to_s]
+
+  sessions.each do |session|
+
+    # fetch dates
+    session_url = "http://164.100.47.132/Members_Attendance/datewise_attendance.aspx?vsessionno=#{session}"
+
+    browser.goto session_url
+
+    date_rows = browser.body.table(:id, "DataGrid1").rows
+
+    date_rows.each_with_index do |row, index|
+      next if index == 0
+      month_name_html = row.tds.first.html
+      p "*********************************************************"
+      p Nokogiri::HTML(month_name_html).content.strip
+      p "*********************************************************"
+
+      row.tds.last.as.each do |a|
+        a.click
+
+        body = browser.body
+ 
+        ls_site = "http://164.100.47.132/Members_Attendance/datewise_results.aspx?xlok=15&vsession=4&vdoa=5/7/2010"
+
+        browser.goto ls_site
+
+        total_links = browser.body.table(:id, "DataGrid1").rows.first.as.size
+
+
+        (1..total_links).each do |i|
+          
+          date_html = body.span(:id, "lb_date").html 
+          p Nokogiri::HTML(date_html).content.strip
+          p "====================================="
+
+          rows = body.table(:id,"DataGrid1").rows
+          rows.each_with_index do |row, index|
+            next if index == 0 || index == 1 || index == (rows.size - 1)
+            row.tds.each_with_index do |td, index| 
+              cell_html = td.html
+              cell_content = Nokogiri::HTML(cell_html).content.strip
+              
+              column_name = case index
+                            when 0 then "Div Number : "
+                            when 1 then "Name : "
+                            when 2 then "Attendance : "
+                            end
+
+              p column_name + cell_content
+            end
+          end
+          next_link = body.table(:id,"DataGrid1").rows.first.as[i-1]
+          next_link.click
+
+          body = browser.body
+        end
+      end
+    end
+  
+  end
 end
+
 
 
