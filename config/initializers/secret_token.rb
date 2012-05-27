10cb9913f404b5734baab7076d89a3dc3e67dae9
Jigar Patel <jagira@gmail.com>
2012-05-27 12:32:08 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tiedot::Application.config.secret_token = '52877b063353fe7780ae75a58e9cbbf366162b5ec53d8848c4d386fde261473be8934e351097f7c5904c3bfc40a93c3df890efca8ff9a207ae227404e261fcb0'
